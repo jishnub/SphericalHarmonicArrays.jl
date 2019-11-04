a2fbc0b11a3a42fb29c3d3fd8eea2167ebbbdbfc
Jishnu Bhattacharya <jishnuonline@gmail.com>
2019-11-04 16:52:25 +0400

    get and set index working

--- src/SphericalHarmonicArrays.jl
+++ src/SphericalHarmonicArrays.jl
@@ -1,5 +1,224 @@
 module SphericalHarmonicArrays
+using Reexport
+import Base: tail, @propagate_inbounds
 
-greet() = print("Hello World!")
+@reexport using SphericalHarmonicModes
+import SphericalHarmonicModes: ModeRange
+
+export SHArray,SHVector, modes, SizeMismatchArrayModeError, 
+SizeMismatchError, MismatchedDimsError, UnexpectedAxisTypeError,
+NotAnSHAxisError
+
+struct SizeMismatchArrayModeError <: Exception
+	dim :: Int
+	arraylength :: Int
+	modelength :: Int
+end
+
+struct SizeMismatchError <: Exception
+	dim :: Int
+	arraylength :: Int
+	axislength :: Int
+end
+
+struct MismatchedDimsError <: Exception
+	M :: Int
+	N :: Int
+end
+
+struct UnexpectedAxisTypeError <: Exception
+	dim :: Int
+	texp :: Type
+	tgot :: Type
+end
+
+struct NotAnSHAxisError <: Exception end
+
+Base.showerror(io::IO, e::SizeMismatchArrayModeError) = print(io,
+	"Size of array does not match the number of modes along dimension $(e.dim). "*
+	"Size of array is $(e.arraylength) whereas the number of modes is $(e.modelength)")
+
+Base.showerror(io::IO, e::SizeMismatchError) = print(io,
+	"Size of array does not match the size of axis specified along dimension $(e.dim). "*
+	"Size of array is $(e.arraylength) whereas the size of axis is $(e.axislength)")
+
+Base.showerror(io::IO, e::MismatchedDimsError) = print(io,
+	"The array has $(e.N) dimensions but $(e.M) shperical harmonic axes specified")
+
+Base.showerror(io::IO, e::UnexpectedAxisTypeError) = print(io,
+	"Expected type $(e.text) along dimension $(e.dim) but got $(e.tgot)")
+
+Base.showerror(io::IO, e::NotAnSHAxisError) = print(io,
+	"Attempted to index into a non-SH axis with a mode Tuple")
+
+const AxisType = Union{ModeRange,AbstractUnitRange}
+
+struct SHArray{T,N,AA<:AbstractArray{T,N},TM<:Tuple{Vararg{<:AxisType,N}},NSH} <: AbstractArray{T,N}
+	parent :: AA
+	modes :: TM
+	SHdims :: NTuple{NSH,Int}
+
+	function SHArray{T,N,AA,TM,NSH}(arr,modes,SHdims) where {T,N,AA,TM,NSH}
+
+		NSH <= N || throw(MismatchedDimsError(NSH,N))
+
+		for (dim,mode) in enumerate(modes)
+			if dim in SHdims && !isa(mode,ModeRange)
+				throw(UnexpectedAxisTypeError(dim,ModeRange,typeof(mode)))
+			elseif !(dim in SHdims) && mode isa ModeRange
+				throw(UnexpectedAxisTypeError(dim,typeof(axes(arr,dim)),typeof(mode)))
+			end 
+			if size(arr,dim) != length(mode)
+				if dim in SHdims
+					throw(SizeMismatchArrayModeError(dim,size(arr,dim),length(mode)))
+				else
+					throw(SizeMismatchError(dim,size(arr,dim),length(mode)))
+				end
+			end
+		end
+		new{T,N,AA,TM,NSH}(arr,modes,SHdims)
+	end
+end
+
+const SHVector{T,AA,M<:Tuple{ModeRange}} = SHArray{T,1,AA,M,1}
+
+function SHArray(arr::AbstractArray{T,N},modes::Tuple{Vararg{<:AxisType,N}},
+	SHdims::NTuple{N,Int}) where {T<:Number,N}
+	SHArray{T,N,typeof(arr),typeof(modes),N}(arr,modes,SHdims)
+end
+
+function SHArray(arr::AbstractArray{T,N},modes::Tuple{Vararg{<:AxisType,N}},
+	SHdims::NTuple{NSH,Int}) where {T<:Number,N,NSH}
+	SHArray{T,N,typeof(arr),typeof(modes),NSH}(arr,modes,SHdims)
+end
+
+function SHArray(arr::AbstractArray{T,N},SHModes::Tuple{Vararg{<:AxisType,NSH}},
+	SHdims::NTuple{NSH,Int}) where {T<:Number,N,NSH}
+	allmodes = Vector{AxisType}(undef,N)
+	allmodes .= axes(arr)
+	for (d,m) in zip(SHdims,SHModes)
+		allmodes[d] = m
+	end
+	modes = Tuple(allmodes)
+	SHArray{T,N,typeof(arr),typeof(modes),length(SHdims)}(arr,modes,SHdims)
+end
+
+function SHArray(arr::AbstractArray,modes::Tuple)
+	SHdims = Tuple(dim for dim in 1:ndims(arr) if modes[dim] isa ModeRange)
+	SHArray(arr,modes,SHdims)
+end
+
+SHArray(arr::AbstractArray) = SHArray(arr,axes(arr))
+SHArray(arr::AbstractVector,mode::ModeRange) = SHArray(arr,(mode,))
+SHArray(arr::AbstractVector,mode::ModeRange,SHdims::Tuple{Int}) = SHArray(arr,(mode,),SHdims)
+
+# These constructors allocate an array of an appropriate size
+SHArray{T}(mode::ModeRange) where {T<:Number} = SHArray(zeros(T,length(mode)),(mode,))
+SHArray(mode::ModeRange) = SHArray{ComplexF64}(mode)
+function SHArray{T}(modes::Vararg{<:AxisType,N}) where {T<:Number,N}
+	ax = Tuple(length(m) for m in modes)
+	SHArray(zeros(T,ax),modes)
+end
+SHArray{T}(modes::Tuple{Vararg{<:AxisType}}) where {T<:Number} = SHArray{T}(modes...)
+SHArray(modes::Vararg{<:AxisType}) = SHArray{ComplexF64}(modes)
+SHArray(modes::Tuple{Vararg{<:AxisType}}) = SHArray{ComplexF64}(modes)
+function SHArray{T}(modes::Tuple{Vararg{<:AxisType}},SHdims::Tuple{Vararg{Int}}) where {T<:Number}
+	ax = Tuple(length(m) for m in modes)
+	SHArray(zeros(T,ax),modes,SHdims)
+end
+SHArray(modes::Tuple{Vararg{<:AxisType}},SHdims::Tuple{Vararg{Int}}) = SHArray{ComplexF64}(modes,SHdims)
+SHArray{T}(mode::ModeRange,SHdims::Tuple{Int}) where {T<:Number} = SHArray(zeros(T,length(mode)),(mode,),SHdims)
+SHArray(mode::ModeRange,SHdims::Tuple{Int}) = SHArray{ComplexF64}(mode,SHdims)
+
+# Convenience constructors
+SHVector(arr::AbstractVector,modes::ModeRange) = SHArray(arr,(modes,),(1,))
+SHVector(arr::AbstractVector,modes::Tuple{ModeRange}) = SHArray(arr,modes,(1,))
+# Automatically allocate a vector of an appropriate size
+SHVector{T}(mode::ModeRange) where {T<:Number} = SHArray(zeros(T,length(mode)),(mode,),(1,))
+SHVector(mode::ModeRange) = SHArray{ComplexF64}(mode)
+
+# Add to Base methods
+
+@inline Base.parent(s::SHArray) = s.parent
+@inline modes(s::SHArray) = s.modes
+
+Base.size(s::SHArray) = size(parent(s))
+Base.axes(s::SHArray) = axes(parent(s))
+
+Base.fill!(s::SHArray,x) = fill!(parent(s),x)
+
+# Indexing 
+
+Base.IndexStyle(::Type{<:SHArray}) = IndexLinear()
+
+# Convert modes to a linear index along each axis
+@inline compute_flatinds(modes,inds::Integer) = inds
+@inline function compute_flatinds(modes::Tuple{Vararg{Any,1}},inds::Tuple{Integer,Integer})
+	modeindex(modes[1],inds)
+end
+@inline function compute_flatinds(::AbstractUnitRange,::Tuple{Integer,Integer})
+	throw(NotAnSHAxisError())
+end
+@inline function compute_flatinds(modes::ModeRange,inds::Tuple{Integer,Integer})
+	modeindex(modes,inds)
+end
+@inline function compute_flatinds(modes::Tuple,inds::Tuple{Vararg{Union{Integer,Tuple}}})
+	(compute_flatinds(modes[1],inds[1]),compute_flatinds(tail(modes),tail(inds))...)
+end
+@inline compute_flatinds(::Tuple{},::Tuple{}) = ()
+@inline compute_flatinds(s::SHArray,inds::Union{Integer,Tuple}) = compute_flatinds(modes(s),inds)
+
+
+# getindex
+
+@inline @propagate_inbounds function Base.getindex(s::SHArray{<:Number,N},
+	inds::Vararg{Union{Integer,Tuple{Integer,Integer}},N}) where {N}
+
+	parentinds = compute_flatinds(s,inds)
+	@boundscheck checkbounds(s, parentinds...)
+	@inbounds ret = parent(s)[parentinds...]
+	ret
+end
+
+# These method passes the indices to the parent
+@inline @propagate_inbounds function Base.getindex(s::SHArray{<:Number,N},
+	inds::Vararg{Integer,N}) where {N}
+
+	@boundscheck checkbounds(s, inds...)
+	@inbounds ret = parent(s)[inds...]
+	ret
+end
+
+@inline @propagate_inbounds function Base.getindex(s::SHArray,ind::Integer)
+	@boundscheck checkbounds(s, ind)
+	@inbounds ret = parent(s)[ind]
+	ret
+end
+
+# setindex
+
+@inline @propagate_inbounds function Base.setindex!(s::SHArray{<:Number,N},val,
+	inds::Vararg{Union{Integer,Tuple{Integer,Integer}},N}) where {N}
+
+	parentinds = compute_flatinds(s,inds)
+	@boundscheck checkbounds(s, parentinds...)
+	@inbounds parent(s)[parentinds...] = val
+	val
+end
+
+# These methods pass the indices to the parent
+@inline @propagate_inbounds function Base.setindex!(s::SHArray{<:Number,N},val,
+	inds::Vararg{Integer,N}) where {N}
+
+	@boundscheck checkbounds(s, inds...)
+	@inbounds parent(s)[inds...] = val
+	val
+end
+
+@inline @propagate_inbounds function Base.setindex!(s::SHArray,val,ind::Integer)
+	@boundscheck checkbounds(s, ind)
+	@inbounds parent(s)[ind] = val
+	val
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,243 @@
 using SphericalHarmonicArrays
 using Test
 
-@testset "SphericalHarmonicArrays.jl" begin
-    # Write your own tests here.
+@testset "Constructors" begin
+	mode = st(0:1,0:0)
+	@testset "normal" begin
+	    arr = zeros(ComplexF64,length(mode))
+	    @test SHArray(arr) == arr
+	end
+	@testset "one SH axis" begin
+		arr = zeros(ComplexF64,length(mode))
+	    @test SHArray(mode) == arr
+	    @test SHArray((mode,)) == arr
+	    @test SHArray(mode,(1,)) == arr
+	    @test SHArray((mode,),(1,)) == arr
+	    @test SHVector(mode) == arr
+	    @test SHVector{ComplexF64}(mode) == arr
+	    @test SHVector{Float64}(mode) == real(arr)
+	    @test SHArray(arr,mode) == arr
+	    @test SHArray(arr,(mode,)) == arr
+	    @test SHArray(arr,mode,(1,)) == arr
+	    @test SHArray(arr,(mode,),(1,)) == arr
+	    @test SHVector(arr,mode) == arr
+
+	    @testset "Errors" begin
+	    	@test_throws UnexpectedAxisTypeError SHArray(mode,(2,))
+	    end
+	end
+    @testset "2D mixed axes" begin
+    	@testset "first SH" begin
+		    arr = zeros(ComplexF64,length(mode),2)
+		    ax = (mode,1:2)
+		    SHdims = (1,)
+		    @test SHArray(ax) == arr
+		    @test SHArray{ComplexF64}(ax) == arr
+		    @test SHArray{Float64}(ax) == real(arr)
+		    @test SHArray(ax,SHdims) == arr
+		    @test SHArray{ComplexF64}(ax,SHdims) == arr
+		    @test SHArray{Float64}(ax,SHdims) == real(arr)
+		    @test SHArray(arr,ax) == arr
+		    @test SHArray(arr,ax,SHdims) == arr
+
+		    @testset "Errors" begin
+		    	@test_throws MismatchedDimsError SHArray(ax,(1,2,3))
+		    	@test_throws UnexpectedAxisTypeError SHArray(ax,(2,))
+		    end
+		end
+		@testset "second SH" begin
+			arr = zeros(ComplexF64,2,length(mode),)
+			ax = (1:2,mode)
+			SHdims = (2,)
+		    @test SHArray(ax) == arr
+		    @test SHArray{ComplexF64}(ax) == arr
+		    @test SHArray{Float64}(ax) == real(arr)
+		    @test SHArray(ax,SHdims) == arr
+		    @test SHArray{ComplexF64}(ax,SHdims) == arr
+		    @test SHArray{Float64}(ax,SHdims) == real(arr)
+		    @test SHArray(arr,ax) == arr
+		    @test SHArray(arr,ax,SHdims) == arr
+		    
+		    @testset "Errors" begin
+		    	@test_throws MismatchedDimsError SHArray(ax,(1,2,3))
+		    	@test_throws UnexpectedAxisTypeError SHArray(ax,(1,))
+		    	@test_throws SizeMismatchArrayModeError SHArray(arr,(1:2,st(s_range(mode))),(2,))
+		    	@test_throws SizeMismatchArrayModeError SHArray(arr,(1:2,st(s_range(mode))))
+		    	@test_throws SizeMismatchError SHArray(arr,(1:3,st(s_range(mode))),(2,))
+		    	@test_throws SizeMismatchError SHArray(arr,(1:3,st(s_range(mode))))
+		    end
+		end
+		@testset "both SH" begin
+			arr = zeros(ComplexF64,length(mode),length(mode))
+			ax = (mode,mode)
+			SHdims = (1,2)
+		    @test SHArray(ax) == arr
+		    @test SHArray{ComplexF64}(ax) == arr
+		    @test SHArray{Float64}(ax) == real(arr)
+		    @test SHArray(ax,SHdims) == arr
+		    @test SHArray{ComplexF64}(ax,SHdims) == arr
+		    @test SHArray{Float64}(ax,SHdims) == real(arr)
+		    @test SHArray(arr,ax) == arr
+		    @test SHArray(arr,ax,SHdims) == arr
+
+		    @testset "Errors" begin
+		    	@test_throws MismatchedDimsError SHArray(ax,(1,2,3))
+		    	@test_throws UnexpectedAxisTypeError SHArray{Float64}(ax,(1,))
+		    	@test_throws UnexpectedAxisTypeError SHArray{Float64}(ax,(2,))
+		    	ax = (st(s_range(mode)),st(s_range(mode)))
+		    	@test_throws SizeMismatchArrayModeError SHArray(arr,ax,(1,2))
+		    	@test_throws SizeMismatchArrayModeError SHArray(arr,ax)
+		    end
+		end
+	end
+end
+
+@testset "Indexing" begin
+	mode = st(0:1,0:0)
+	@testset "1D SH" begin
+	    v = SHVector(mode)
+	    @testset "getindex" begin
+	    	@testset "linear" begin
+		    	for i in eachindex(v)
+			    	@test v[i] == 0
+			    end
+	    	end
+	    	@testset "mode" begin
+			    for m in mode
+			    	@test v[m] == 0
+			    end
+	    	end
+	    end
+	    @testset "setindex" begin
+	    	@testset "linear" begin
+			    for i in eachindex(v)
+			    	v[i] = i
+			    	@test v[i] == i
+			    end
+	    	end
+	    	@testset "mode" begin
+			    for (i,m) in enumerate(mode)
+			    	v[m] = i
+			    	@test v[m] == i
+			    end
+	    	end
+	    end
+	end
+	@testset "1D normal" begin
+	    v = SHArray(zeros(length(mode)))
+	    @testset "getindex" begin
+	    	@testset "linear" begin
+		    	for i in eachindex(v)
+			    	@test v[i] == 0
+			    end
+			end
+			@testset "mode" begin
+			    for m in mode
+			    	@test_throws NotAnSHAxisError v[m]
+			    end
+	    	end
+	    end
+	    @testset "setindex" begin
+	    	@testset "linear" begin
+			    for i in eachindex(v)
+			    	v[i] = i
+			    	@test v[i] == i
+			    end
+	    	end
+	    	@testset "mode" begin
+			    for (i,m) in enumerate(mode)
+			    	@test_throws NotAnSHAxisError v[m] = i
+			    end
+	    	end
+	    end
+	end
+	@testset "2D mixed axes" begin
+		@testset "first SH" begin
+		    v = SHArray((mode,1:2))
+		    @testset "getindex" begin
+		    	@testset "linear" begin
+			    	for i in eachindex(v)
+				    	@test v[i] == 0
+				    end
+		    	end
+		    	@testset "mode" begin
+				    for ind2 in axes(v,2), m in mode
+				    	@test v[m,ind2] == 0
+				    end
+		    	end
+		    end
+		    @testset "setindex" begin
+		    	@testset "linear" begin
+				    for i in eachindex(v)
+				    	v[i] = i
+				    	@test v[i] == i
+				    end
+		    	end
+		    	@testset "mode" begin
+				    for ind2 in axes(v,2),(i,m) in enumerate(mode)
+				    	v[m,ind2] = i
+				    	@test v[m,ind2] == i
+				    end
+		    	end
+		    end
+		end
+		@testset "second SH" begin
+		    v = SHArray((1:2,mode))
+		    @testset "getindex" begin
+		    	@testset "linear" begin
+			    	for i in eachindex(v)
+				    	@test v[i] == 0
+				    end
+		    	end
+		    	@testset "mode" begin
+				    for (i,m) in enumerate(mode),ind1 in axes(v,1)
+				    	@test v[ind1,m] == 0
+				    end
+		    	end
+		    end
+		    @testset "setindex" begin
+		    	@testset "linear" begin
+				    for i in eachindex(v)
+				    	v[i] = i
+				    	@test v[i] == i
+				    end
+		    	end
+		    	@testset "mode" begin
+				    for (i,m) in enumerate(mode),ind1 in axes(v,1)
+				    	v[ind1,m] = i
+				    	@test v[ind1,m] == i
+				    end
+		    	end
+		    end
+		end
+		@testset "both SH" begin
+		    v = SHArray((mode,mode))
+		    @testset "getindex" begin
+		    	@testset "linear" begin
+			    	for i in eachindex(v)
+				    	@test v[i] == 0
+				    end
+		    	end
+		    	@testset "mode" begin
+				    for m2 in mode,m1 in mode
+				    	@test v[m1,m2] == 0
+				    end
+		    	end
+		    end
+		    @testset "setindex" begin
+		    	@testset "linear" begin
+				    for i in eachindex(v)
+				    	v[i] = i
+				    	@test v[i] == i
+				    end
+		    	end
+		    	@testset "mode" begin
+				    for (i,m2) in enumerate(mode),m1 in mode
+				    	v[m1,m2] = i
+				    	@test v[m1,m2] == i
+				    end
+		    	end
+		    end
+		end
+	end
 end
